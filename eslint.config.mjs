add0dcc74ec4b185fd654b76203e72b18d03f5a2
DeibidSE <davidsimon98@outlook.com>
2025-05-23 19:16:17 +0200

    Update dependencies

---
+++ eslint.config.mjs
@@ -0,0 +1,12 @@
+// @ts-check
+import withNuxt from './.nuxt/eslint.config.mjs'
+
+export default withNuxt(
+	{
+		rules: {
+			'@stylistic/brace-style': ['error', '1tbs', { allowSingleLine: true }],
+			'@stylistic/max-statements-per-line': ['error', { max: 2 }],
+			'@typescript-eslint/no-explicit-any': 'off',
+		},
+	},
+)
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,72 +1,78 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: false },
-  eslint: { lintOnStart: false },
-  experimental: {
-    viewTransition: true
-  },
-  ssr: false,
-  runtimeConfig: {
-    public: {
-      BASE_URL: '/spotify-clone'
-    }
-  },
-  app: {
-    baseURL: '/spotify-clone/',
-    head: {
-      title: 'Spotify Clone',
-      meta: [
-        {
-          charset: 'utf-8'
-        },
-        {
-          name: 'viewport',
-          content: 'width=device-width, initial-scale=1'
-        },
-        {
-          hid: 'description',
-          name: 'description',
-          content: 'Spotify Clone created with Nuxt 3'
-        },
-        {
-          name: 'author',
-          content: 'David Simón - @DeibidSE'
-        }
-      ],
-      link: [
-        {
-          rel: 'icon',
-          type: 'image/x-icon',
-          href: '/spotify-clone/favicon.ico'
-        }
-      ],
-      htmlAttrs: {
-        lang: 'en'
-      }
-    }
-  },
-  modules: [
-    '@nuxtjs/eslint-module',
-    '@nuxtjs/i18n',
-    '@nuxtjs/tailwindcss',
-    '@pinia/nuxt',
-    'nuxt-icons',
-    'pinia-plugin-persistedstate/nuxt',
-    'radix-vue/nuxt'
-  ],
-  i18n: {
-    strategy: 'no_prefix',
-    defaultLocale: 'en',
-    locales: [
-      { code: 'es', language: 'es-ES', name: 'Español de España (European Spanish)', file: 'es.json' },
-      { code: 'en', language: 'en-GB', name: 'English (English)', file: 'en.json' }
-    ],
-    detectBrowserLanguage: {
-      useCookie: true,
-      cookieKey: 'i18n_redirected',
-      redirectOn: 'root'
-    }
-  },
-  css: ['@/assets/css/tailwind.css'],
-  compatibilityDate: '2024-08-24'
+	modules: [
+		'@nuxt/eslint',
+		'@nuxtjs/i18n',
+		'@nuxtjs/tailwindcss',
+		'@pinia/nuxt',
+		'nuxt-icons',
+		'pinia-plugin-persistedstate/nuxt',
+		'radix-vue/nuxt',
+	],
+	ssr: false,
+	devtools: { enabled: false },
+	app: {
+		baseURL: '/spotify-clone/',
+		head: {
+			title: 'Spotify Clone',
+			meta: [
+				{
+					charset: 'utf-8',
+				},
+				{
+					name: 'viewport',
+					content: 'width=device-width, initial-scale=1',
+				},
+				{
+					hid: 'description',
+					name: 'description',
+					content: 'Spotify Clone created with Nuxt 3',
+				},
+				{
+					name: 'author',
+					content: 'David Simón - @DeibidSE',
+				},
+			],
+			link: [
+				{
+					rel: 'icon',
+					type: 'image/x-icon',
+					href: '/spotify-clone/favicon.ico',
+				},
+			],
+			htmlAttrs: {
+				lang: 'en',
+			},
+		},
+	},
+	css: ['@/assets/css/tailwind.css'],
+	runtimeConfig: {
+		public: {
+			BASE_URL: '/spotify-clone',
+		},
+	},
+	experimental: {
+		viewTransition: true,
+	},
+	compatibilityDate: '2024-08-24',
+	eslint: {
+		config: {
+			stylistic: {
+				indent: 'tab',
+			},
+		},
+	},
+	i18n: {
+		strategy: 'no_prefix',
+		defaultLocale: 'en',
+		locales: [
+			{ code: 'es', language: 'es-ES', name: 'Español de España (European Spanish)', file: 'es.json' },
+			{ code: 'en', language: 'en-GB', name: 'English (English)', file: 'en.json' },
+		],
+		detectBrowserLanguage: {
+			useCookie: true,
+			cookieKey: 'i18n_redirected',
+			redirectOn: 'root',
+		},
+	},
 })
